816be4f74697e85dc1ff49bff168505ea2f0a7c2
Angus Morrison <angus@howtocodeit.com>
2026-04-02 16:04:24 +0900

    refactor: improve BlockGenerator mockability (#15)

--- src/block_generator.rs
+++ src/block_generator.rs
@@ -5,12 +5,12 @@ use crate::block::BlockType;
 
 /// Randomly generates new blocks based on the supplied RNG.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct BlockGenerator<R> {
+pub struct BlockGenerator<R, S> {
     rng: R,
-    sampler: Uniform<u8>,
+    sampler: S,
 }
 
-impl<R> BlockGenerator<R> {
+impl<R> BlockGenerator<R, Uniform<u8>> {
     pub fn new(rng: R) -> Self {
         let sampler = Uniform::new_inclusive(1, BlockType::COUNT)
             .unwrap_or_else(|_| panic!("uniform sampler was invalid for 1..={}", BlockType::COUNT));
@@ -18,7 +18,7 @@ impl<R> BlockGenerator<R> {
     }
 }
 
-impl<R: Rng> BlockGenerator<R> {
+impl<R: Rng, S: Distribution<u8>> BlockGenerator<R, S> {
     /// Generate a new block.
     pub fn block(&mut self) -> BlockType {
         match self.sampler.sample(&mut self.rng) {
@@ -34,6 +34,97 @@ impl<R: Rng> BlockGenerator<R> {
                 BlockType::COUNT,
                 i
             ),
+        }
+    }
+}
+
+#[cfg(test)]
+mod block_generator_tests {
+    use super::*;
+    use rand_distr::Distribution;
+
+    struct MockSampler(u8);
+
+    impl Distribution<u8> for MockSampler {
+        fn sample<R: Rng + ?Sized>(&self, _rng: &mut R) -> u8 {
+            self.0
+        }
+    }
+
+    mod block_tests {
+        use super::*;
+
+        #[test]
+        #[should_panic]
+        fn when_sampler_produces_out_of_range_value_panics() {
+            BlockGenerator {
+                rng: rand::rng(),
+                sampler: MockSampler(0),
+            }
+            .block();
+        }
+
+        #[test]
+        fn when_sampler_produces_1_returns_i() {
+            let mut generator = BlockGenerator {
+                rng: rand::rng(),
+                sampler: MockSampler(1),
+            };
+            assert_eq!(generator.block(), BlockType::I);
+        }
+
+        #[test]
+        fn when_sampler_produces_2_returns_j() {
+            let mut generator = BlockGenerator {
+                rng: rand::rng(),
+                sampler: MockSampler(2),
+            };
+            assert_eq!(generator.block(), BlockType::J);
+        }
+
+        #[test]
+        fn when_sampler_produces_3_returns_l() {
+            let mut generator = BlockGenerator {
+                rng: rand::rng(),
+                sampler: MockSampler(3),
+            };
+            assert_eq!(generator.block(), BlockType::L);
+        }
+
+        #[test]
+        fn when_sampler_produces_4_returns_o() {
+            let mut generator = BlockGenerator {
+                rng: rand::rng(),
+                sampler: MockSampler(4),
+            };
+            assert_eq!(generator.block(), BlockType::O);
+        }
+
+        #[test]
+        fn when_sampler_produces_5_returns_s() {
+            let mut generator = BlockGenerator {
+                rng: rand::rng(),
+                sampler: MockSampler(5),
+            };
+            assert_eq!(generator.block(), BlockType::S);
+        }
+
+        #[test]
+        fn when_sampler_produces_6_returns_t() {
+            let mut generator = BlockGenerator {
+                rng: rand::rng(),
+                sampler: MockSampler(6),
+            };
+            assert_eq!(generator.block(), BlockType::T);
+        }
+
+        #[test]
+        fn when_sampler_produces_7_returns_z() {
+            let mut generator = BlockGenerator {
+                rng: rand::rng(),
+                sampler: MockSampler(7),
+            };
+            assert_eq!(generator.block(), BlockType::Z);
         }
     }
 }
